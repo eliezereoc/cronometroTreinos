38f65a6ec2eb97d03820d96f6bc35da04529ed6e
eliezereoc <eliezeroc@gmail.com>
2021-08-26 23:27:57 -0300

    Implementado componete configuração

---
+++ crometroExercicios/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ConfigComponent } from './config/config.component';
+import { TimerComponent } from './timer/timer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ConfigComponent,
+    TimerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
